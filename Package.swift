5549e4506bef5b11ab4e681f42012c510f751e50
Diego <diego.meire@outlook.com>
2021-10-23 00:14:28 -0300

    Adding public headers

--- Package.swift
+++ Package.swift
@@ -18,12 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "UnityFramework",
-            dependencies: [],
-			publicHeadersPath: "Headers"),
-        .testTarget(
-            name: "UnityFrameworkTests",
-            dependencies: ["UnityFramework"]),
+        .binaryTarget(
+            name: "MyFramework", 
+            path: "MyFramework.xcframework")
     ]
 )
