5977ac5cdf527a6d7c73fad44e9e70dbb6140ae3
Diego <diego.meire@outlook.com>
2021-10-23 00:02:05 -0300

    Adding public headers

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UnityFramework",
-            dependencies: []),
+            dependencies: [],
+			publicHeadersPath: "include"),
         .testTarget(
             name: "UnityFrameworkTests",
             dependencies: ["UnityFramework"]),
